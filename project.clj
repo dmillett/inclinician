0ac7381d8c1c817ff2aad32f2b3e536653f6c1b4
dave <dbmillett@gmail.com>
2020-12-01 21:05:05 -0600

    readme: added covid thoughts and why we need a lot more data.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU Lesser General Public License, version 2.1"
             :url "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [lein-licenses "0.2.2"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [lein-licenses "0.2.2"]
+                 [clash "1.5.1"]]
   :repl-options {:init-ns inclinician.core})
